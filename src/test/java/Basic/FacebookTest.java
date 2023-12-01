95a4c7e3e48ddbbfc8d2e3fdb484f9325f5c2dfa
Supriyahv9 <supriyahv9@gmail.com>
2023-12-01 17:36:29 +0530

    added

---
+++ src/test/java/Basic/FacebookTest.java
@@ -0,0 +1,19 @@
+package Basic;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class FacebookTest {
+
+	
+	@Test
+	public void ActitimeTest() {
+	WebDriver driver = new ChromeDriver();
+	driver.get("https://www.facebook.com/");
+	String actualtitle = driver.getTitle();
+	System.out.println(actualtitle);
+	System.out.println("Hello");
+		
+	}
+}
